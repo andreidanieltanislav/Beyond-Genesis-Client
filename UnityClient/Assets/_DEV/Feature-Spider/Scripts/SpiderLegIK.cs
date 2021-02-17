566cd8cd76a2373320a655f6c2c3b6c4132cce8e
Cosmin Sterian <c.cristian.sterian@gmail.com>
2021-02-17 22:55:10 +0200

    Handle leg position interpolation in legs controller
    UPB-Graphics/Beyond-Genesis-Client#3

--- UnityClient/Assets/_DEV/Feature-Spider/Scripts/SpiderLegIK.cs
+++ UnityClient/Assets/_DEV/Feature-Spider/Scripts/SpiderLegIK.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SpiderLegIK : MonoBehaviour
 {
@@ -11,22 +12,27 @@ public class SpiderLegIK : MonoBehaviour
     public float lowerLegLength = 0.5f;
 
     public float delta = 0.01f;
+    public int iterations = 10;
+    public float legSpeed = 10f;
+    public float legRaiseHeight = 0.5f;
+    public float timeToMoveLeg = 1f;
 
-    public SpiderLegIKTarget LegIKTarget
+    public SpiderLegIKTarget CurrentIKTarget
     {
-        get => _legIKTarget;
+        get => _currentIKTarget;
         set
         {
-            _legIKTarget = value;
+            _previousIKTarget = _currentIKTarget;
+            _currentIKTarget = value;
             UpdateLegPosition();
         }
     }
 
-    private SpiderLegIKTarget _legIKTarget;
+    private SpiderLegIKTarget _currentIKTarget;
+    private SpiderLegIKTarget _intermediateIKTarget;
+    private SpiderLegIKTarget _previousIKTarget;
+
     private float _totalLength;
-    private Vector3 _initialUpperLegLocalPos;
-    private Vector3 _initialMiddleLegLocalPos;
-    private Vector3 _initialLowerLegLocalPos;
     private Quaternion _lowerLegRotation;
 
     private Vector3 UpperLegEnd => upperLeg.position + upperLeg.forward * upperLegLength;
@@ -35,10 +41,10 @@ public class SpiderLegIK : MonoBehaviour
 
     private void Awake()
     {
-        _initialUpperLegLocalPos = upperLeg.localPosition;
-        _initialMiddleLegLocalPos = middleLeg.localPosition;
-        _initialLowerLegLocalPos = lowerLeg.localPosition;
         _totalLength = upperLegLength + middleLegLength + lowerLegLength;
+        _previousIKTarget = new SpiderLegIKTarget {Normal = Vector3.zero, Position = Vector3.zero};
+        _intermediateIKTarget = _previousIKTarget;
+        Debug.Log($"Intermediate {_intermediateIKTarget.Position}");
     }
 
     private void Start()
@@ -48,36 +54,48 @@ public class SpiderLegIK : MonoBehaviour
 
     private void Update()
     {
-        Vector3 targetPosition = _legIKTarget.Position;
+        Vector3 targetPosition = _currentIKTarget.Position;
         if (_totalLength <= (targetPosition - upperLeg.position).magnitude)
         {
             StretchLeg();
             return;
         }
 
-        if ((targetPosition - LowerLegEnd).magnitude > delta)
-        {
+        // int ikIterations = iterations;
+        // while ((targetPosition - LowerLegEnd).magnitude > delta && ikIterations > 0)
+        // {
+            //ComputeIntermediatePosition();
             SolveIK();
-            // TODO: Freeze leg, follow target when distance is greater, Pole
-        }
+            // TODO: Pole
+        //     ikIterations--;
+        // }
     }
-    
+
     private void UpdateLegPosition()
     {
-        lowerLeg.position = _legIKTarget.Position + _legIKTarget.Normal * lowerLegLength;
-        lowerLeg.LookAt(_legIKTarget.Position);
+        Debug.Log("Lower leg position updated");
+        lowerLeg.position = _currentIKTarget.Position + _currentIKTarget.Normal * lowerLegLength;
+        lowerLeg.LookAt(_currentIKTarget.Position);
         _lowerLegRotation = lowerLeg.rotation; // Used to freeze rotation of lower leg
         SolveIK();
     }
 
     private void SolveIK()
     {
-        BackwardIK(ForwardIK());
+        int ikIterations = iterations;
+        // while ((_currentIKTarget.Position - LowerLegEnd).magnitude > delta && ikIterations > 0)
+        while (ikIterations > 0)
+        {
+            BackwardIK(ForwardIK());
+            ikIterations--;
+        }
     }
 
     private Vector3[] ForwardIK()
     {
-        Vector3 intermediateLowerLegPos = _legIKTarget.Position + _legIKTarget.Normal * lowerLegLength;
+        // Vector3 intermediateLowerLegPos =
+        //     _intermediateIKTarget.Position + _intermediateIKTarget.Normal * lowerLegLength;
+        Vector3 intermediateLowerLegPos = _currentIKTarget.Position + _currentIKTarget.Normal * lowerLegLength;
         Vector3 intermediateMiddleLegPos = intermediateLowerLegPos -
                                            (intermediateLowerLegPos - middleLeg.position).normalized * middleLegLength;
 
@@ -97,19 +115,79 @@ public class SpiderLegIK : MonoBehaviour
     // To be used when target is out of reach
     private void StretchLeg()
     {
-        // TODO: Further experiment
-        // upperLeg.LookAt(target);
-        // upperLeg.localPosition = _initialUpperLegLocalPos;
-        // middleLeg.LookAt(target);
-        // middleLeg.localPosition = _initialMiddleLegLocalPos;
-        // lowerLeg.LookAt(target);
-        // lowerLeg.localPosition = _initialLowerLegLocalPos;
-        Vector3 targetPosition = _legIKTarget.Position;
+        Vector3 targetPosition = _currentIKTarget.Position;
         upperLeg.LookAt(targetPosition);
         middleLeg.position = UpperLegEnd;
         middleLeg.LookAt(targetPosition);
         lowerLeg.position = MiddleLegEnd;
         lowerLeg.LookAt(targetPosition);
+    }
+
+    private void ComputeIntermediatePosition()
+    {
+        Vector3 targetPos = _currentIKTarget.Position;
+        Vector3 targetNormal = _currentIKTarget.Normal;
+        Vector3 previousPos = _previousIKTarget.Position;
+        Vector3 previousNormal = _previousIKTarget.Normal;
+        Vector3 middlePos = Vector3.Lerp(previousPos, targetPos, 0.5f);
+        Vector3 middleNormal = Vector3.Lerp(previousNormal, targetNormal, 0.5f);
+        float totalDistance = (targetPos - previousPos).magnitude;
+        if (totalDistance < delta)
+        {
+            return;
+        }
+
+        /*
+         * Compute currentDistance removing the elevation of the foot.
+         * Done by creating a plane between the previous and target points
+         * and projecting the current position on the plane.
+         */
+        Plane plane = new Plane(-middleNormal, middlePos);
+        Vector3 projectedIntermediatePos = plane.ClosestPointOnPlane(_intermediateIKTarget.Position);
+        // Debug.Log($"Projected prev: {plane.ClosestPointOnPlane(previousPos)}, " +
+        //           $"target: {plane.ClosestPointOnPlane(targetPos)}; " +
+        //           $"actual prev: {previousPos}, target: {targetPos}");
+        float currentDistance = (targetPos - projectedIntermediatePos).magnitude;
+
+
+        // Debug.Log($"targetPos: {targetPos}, currentPos: {_intermediateIKTarget.Position}, " +
+        //           $"distance left: {currentDistance}, total distance: {totalDistance} " +
+        //           $"prevPos: {previousPos}");
+
+        float t = Mathf.Clamp01(currentDistance * 2 / totalDistance);
+        Vector3 nextNormal, nextPos;
+        Vector3 raisedMiddlePos = middlePos + middleNormal * legRaiseHeight;
+        if (currentDistance > totalDistance / 2)
+        {
+            nextPos = Vector3.Slerp(previousPos, raisedMiddlePos, t);
+            nextNormal = Vector3.Slerp(previousNormal, middleNormal, t);
+        }
+        else
+        {
+            nextPos = Vector3.Slerp(raisedMiddlePos, targetPos, t);
+            nextNormal = Vector3.Slerp(middleNormal, targetNormal, t);
+        }
+
+
+        // float t = currentDistance / totalDistance;
+        // Vector3 nextNormal = Vector3.Slerp(previousNormal, targetNormal, t);
+        // if (currentDistance > totalDistance / 4)
+        // {
+        //     // Raise the leg
+        //     targetPos += Vector3.Slerp(previousNormal, targetNormal, 0.5f) * legRaiseHeight;
+        // }
+        //
+        // Vector3 nextPos = Vector3.Slerp(_intermediateIKTarget.Position, targetPos, Time.deltaTime * legSpeed);
+
+        // _intermediateIKTarget.Position = nextPos;
+        // _intermediateIKTarget.Normal = nextNormal;
+        _intermediateIKTarget = new SpiderLegIKTarget {Normal = nextNormal, Position = nextPos};
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_currentIKTarget.Position, 0.5f);
     }
 }
 
--- UnityClient/Assets/_DEV/Feature-Spider/Scripts/SpiderLegsController.cs
+++ UnityClient/Assets/_DEV/Feature-Spider/Scripts/SpiderLegsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,12 +8,16 @@ public class SpiderLegsController : MonoBehaviour
 {
     public LegIKTargetPair[] legIKsTargets;
     public float maxLegDistance = 2f;
+    public float legRaiseHeight = 1f;
+    public float timeToMoveLeg = 0.5f;
 
     private List<Collider> _legsColliders;
+    private Dictionary<SpiderLegIK, IEnumerator> _runningLegInterpolations;
 
     private void Awake()
     {
         _legsColliders = GetComponentsInChildren<Collider>().ToList();
+        _runningLegInterpolations = new Dictionary<SpiderLegIK, IEnumerator>(legIKsTargets.Length);
     }
 
     private void LateUpdate()
@@ -26,7 +31,7 @@ public class SpiderLegsController : MonoBehaviour
         // Temporarily put legs on ignore layer, assuming all legs are on same layer initially
         int previousLayer = legIKsTargets[0].legIK.gameObject.layer;
         _legsColliders.ForEach(legCollider => legCollider.gameObject.layer = 2);
-        
+
         foreach (LegIKTargetPair pair in legIKsTargets)
         {
             Transform legTarget = pair.legTarget;
@@ -37,7 +42,7 @@ public class SpiderLegsController : MonoBehaviour
                 pair.legTarget.forward = hit.normal;
             }
         }
-        
+
         // Restore layer
         _legsColliders.ForEach(legCollider => legCollider.gameObject.layer = previousLayer);
     }
@@ -48,15 +53,78 @@ public class SpiderLegsController : MonoBehaviour
         {
             Vector3 targetPosition = pair.legTarget.position;
             Vector3 targetForward = pair.legTarget.forward;
-            float sqrDistanceBetweenLegAndTarget = (pair.legIK.LegIKTarget.Position - targetPosition).sqrMagnitude;
-            
+            SpiderLegIK legIK = pair.legIK;
+            float sqrDistanceBetweenLegAndTarget = (legIK.CurrentIKTarget.Position - targetPosition).sqrMagnitude;
+
             if (sqrDistanceBetweenLegAndTarget > maxLegDistance * maxLegDistance)
             {
                 // Leg is too far away, update its target (position)
                 Debug.Log("Updating leg position");
-                pair.legIK.LegIKTarget = new SpiderLegIKTarget {Normal = targetForward, Position = targetPosition};
+                SpiderLegIKTarget nextTarget = new SpiderLegIKTarget
+                {
+                    Normal = targetForward,
+                    Position = targetPosition
+                };
+                // pair.legIK.CurrentIKTarget = new SpiderLegIKTarget {Normal = targetForward, Position = targetPosition};
+                if (_runningLegInterpolations.ContainsKey(legIK))
+                {
+                    return;
+                    // StopCoroutine(_runningLegInterpolations[legIK]);
+                }
+
+                _runningLegInterpolations[legIK] = InterpolateLegPosition(legIK, legIK.CurrentIKTarget, nextTarget);
+                StartCoroutine(_runningLegInterpolations[legIK]);
             }
         }
+    }
+
+    private IEnumerator InterpolateLegPosition(SpiderLegIK leg, SpiderLegIKTarget previousTarget,
+        SpiderLegIKTarget nextTarget)
+    {
+        Debug.Log("Started coroutine");
+        float elapsedTime = 0f;
+        Vector3 prevPos = previousTarget.Position;
+        Vector3 prevNormal = previousTarget.Normal;
+        Vector3 nextPos = nextTarget.Position;
+        Vector3 nextNormal = nextTarget.Normal;
+        Vector3 middlePos = Vector3.Slerp(prevPos, nextPos, 0.5f);
+        Vector3 middleNormal = Vector3.Slerp(prevNormal, nextNormal, 0.5f);
+        // raisedMiddlePos Might not look like raising at all when walking uneven terrain
+        Vector3 raisedMiddlePos = middlePos + middleNormal * legRaiseHeight;
+
+        Vector3 lastRaisedPos = prevPos;
+
+        while (elapsedTime < timeToMoveLeg)
+        {
+            elapsedTime += Time.deltaTime;
+            // float t = Mathf.Clamp01(elapsedTime * 2 / timeToMoveLeg);
+            float t = Mathf.Clamp01(elapsedTime / timeToMoveLeg);
+            
+            Vector3 newPos, newNormal;
+            if (t < timeToMoveLeg / 2)
+            {
+                newPos = Vector3.Slerp(prevPos, raisedMiddlePos, t * 2);
+                newNormal = Vector3.Slerp(prevNormal, middleNormal, t * 2);
+                lastRaisedPos = newPos;
+            }
+            else
+            {
+                newPos = Vector3.Slerp(lastRaisedPos, nextPos, (t - timeToMoveLeg / 2)*2);
+                newNormal = Vector3.Slerp(middleNormal, nextNormal, (t-timeToMoveLeg/2)*2);
+            }
+
+            // newPos = Vector3.Lerp(prevPos, nextPos, t);
+            // newNormal = Vector3.Lerp(prevNormal, nextNormal, t);
+            
+            leg.CurrentIKTarget = new SpiderLegIKTarget
+            {
+                Normal = newNormal,
+                Position = newPos
+            };
+            yield return null;
+        }
+
+        _runningLegInterpolations.Remove(leg);
     }
 }
 
